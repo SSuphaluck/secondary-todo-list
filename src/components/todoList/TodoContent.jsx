e33778e4319c78c04166e749d1e95bbd95119e0c
SSuphaluck <hibara.and.conun@gmail.com>
2023-03-16 23:54:37 +0700

    feature togle status

--- src/components/todoList/TodoContent.jsx
+++ src/components/todoList/TodoContent.jsx
@@ -1,17 +1,53 @@
-function TodoContent() {
+import axios from 'axios';
+
+function TodoContent(props) {
+  const handleClickDelete = async () => {
+    try {
+      const res = await axios.delete(
+        'http://localhost:8080/todos/' + props.todo.id
+      );
+      props.fetchTodos();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleClickToggle = async () => {
+    try {
+      const res = await axios.put(
+        'http://localhost:8080/todos/' + props.todo.id,
+        { title: props.todo.title, completed: !props.todo.completed }
+      );
+      props.fetchTodos();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="d-flex align-items-center">
       <span
         className="flex-fill"
         role="button"
+        onClick={props.openEdit}
       >
-        Watch movie
+        {props.todo.title}{' '}
       </span>
       <div className="btn-group">
-        <button className="btn btn-outline-secondary">
-          <i className="fa-solid fa-toggle-off" />
+        <button
+          className="btn btn-outline-secondary"
+          onClick={handleClickToggle}
+        >
+          <i
+            className={`fa-solid fa-toggle-${
+              props.todo.completed ? 'on' : 'off'
+            }`}
+          />
         </button>
-        <button className="btn btn-outline-secondary">
+        <button
+          className="btn btn-outline-secondary"
+          onClick={handleClickDelete}
+        >
           <i className="fa-regular fa-trash-can" />
         </button>
       </div>
