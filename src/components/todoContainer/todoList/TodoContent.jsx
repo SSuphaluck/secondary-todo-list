1362742ba0ee1175f502868606a5ebd27a385628
SSuphaluck <hibara.and.conun@gmail.com>
2023-03-18 14:30:37 +0700

    add and deleted todo

--- src/components/todoContainer/todoList/TodoContent.jsx
+++ src/components/todoContainer/todoList/TodoContent.jsx
@@ -1,17 +1,25 @@
-function TodoContent() {
+import { useContext } from 'react';
+import { TodoContext } from '../../../contexts/TodoContext';
+
+function TodoContent({ todo: { title, completed, id } }) {
+  const ctx = useContext(TodoContext);
+
   return (
     <div className="d-flex align-items-center">
       <span
         className="flex-fill"
         role="button"
       >
-        Watch movie
+        {title}
       </span>
       <div className="btn-group">
         <button className="btn btn-outline-secondary">
-          <i className="fa-solid fa-toggle-off" />
+          <i className={`fa-solid fa-toggle-${completed ? 'on' : 'off'}`} />
         </button>
-        <button className="btn btn-outline-secondary">
+        <button
+          className="btn btn-outline-secondary"
+          onClick={() => ctx.deleteTodo(id)}
+        >
           <i className="fa-regular fa-trash-can" />
         </button>
       </div>
---
+++ src/contexts/TodoContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+const TodoContext = createContext();
+
+function TodoContextProvider(props) {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/todos')
+      .then((res) => {
+        setTodos(res.data.todos);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const createTodo = (title) => {
+    axios
+      .post('http://localhost:8080/todos', { title, completed: false })
+      .then((res) => {
+        setTodos([res.data.todo, ...todos]);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const deleteTodo = (id) => {
+    axios
+      .delete(`http://localhost:8080/todos/${id}`)
+      .then(() => {
+        setTodos(todos.filter((item) => item.id !== id));
+        // setTodos((prevTodo) => prevTodo.filter((item) => item.id !== id)); อันนี้แนะนำ
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <TodoContext.Provider value={{ todos, createTodo, deleteTodo }}>
+      {props.children}
+    </TodoContext.Provider>
+  );
+}
+
+export default TodoContextProvider;
+export { TodoContext };
