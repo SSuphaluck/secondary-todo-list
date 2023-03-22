3ac83d4956da1f351ac59e29b8b0b55d7b101100
SSuphaluck <hibara.and.conun@gmail.com>
2023-03-22 22:53:30 +0700

    class-component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
+import { Component } from 'react';
+import axios from 'axios';
 import TodoContainer from './components/todoContainer/TodoContainer';
 import TodoForm from './components/TodoForm';
 
-function App() {
-  return (
-    <div
-      className="container mt-5 mb-3"
-      style={{ maxWidth: 576 }}
-    >
-      <div className="my-4">
-        <TodoForm />
+class App extends Component {
+  state = {
+    todos: []
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:8080/todos')
+      .then((res) => {
+        this.setState({ todos: res.data.todos });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  createTodo = (title) => {
+    axios
+      .post('http://localhost:8080/todos', { title, completed: false })
+      .then((res) => {
+        this.setState({ todos: [res.data.todo, ...this.state.todos] });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div
+        className="container mt-5 mb-3"
+        style={{ maxWidth: 576 }}
+      >
+        <div className="my-4">
+          <TodoForm onSubmit={this.createTodo} />
+        </div>
+        <TodoContainer todos={this.state.todos} />
       </div>
-      <TodoContainer />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,20 +1,41 @@
-function TodoForm() {
-  return (
-    <form>
-      <div className="input-group">
-        <input
-          type="text"
-          className="form-control"
-        />
-        <button className="btn btn-primary">
-          <i className="fa-solid fa-check" />
-        </button>
-        <button className="btn btn-outline-secondary">
-          <i className="fa-solid fa-xmark" />
-        </button>
-      </div>
-    </form>
-  );
+import { Component } from 'react';
+
+class TodoForm extends Component {
+  state = {
+    input: ''
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    //createTodo
+    this.props.onSubmit(this.state.input);
+    this.setState({ input: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            value={this.state.input}
+            onChange={(e) => this.setState({ input: e.target.value })}
+          />
+          <button className="btn btn-primary">
+            <i className="fa-solid fa-check" />
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-secondary"
+            onClick={() => this.setState({ input: '' })}
+          >
+            <i className="fa-solid fa-xmark" />
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default TodoForm;
